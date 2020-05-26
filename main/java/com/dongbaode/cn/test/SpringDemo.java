0a60333ea17a191c2fcf60227016faf143bb2a66
hongdehanye <18101717959@163.com>
2020-05-26 21:40:23 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main/java/com/dongbaode/cn/Dong.java

---
+++ main/java/com/dongbaode/cn/test/SpringDemo.java
@@ -0,0 +1,23 @@
+package com.dongbaode.cn.test;
+
+public class SpringDemo {
+
+    private String num;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+}
